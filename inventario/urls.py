0d5aab63846894c10cba1c45f82086d8fa13a3a6
ivanperez-7 <ivan.perez27@outlook.com>
2025-10-03 15:21:27 -0600

    Modifica app de inventario para usar serializers

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from .views import *
 
 
+router = routers.DefaultRouter()
+router.register('', ProductoViewSet)
+
 urlpatterns = [
-    path('get-tabla-precios-simples/', get_tabla_precios_simples),
+    path('', include(router.urls)),
 ]
--- inventario/views.py
+++ inventario/views.py
@@ -1,57 +1,37 @@
-from django.db import connection
-from rest_framework.decorators import api_view, permission_classes
+from django.db.models import Case, CharField, F, Sum, Value, When
+from django.db.models.functions import Cast, Coalesce, Concat
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
+from .models import *
+from .serializers import *
+from utils.mixins import GetWithOrmMixin
 
-@api_view()
-@permission_classes([IsAuthenticated])
-def get_tabla_precios_simples(request):
-    with connection.cursor() as cursor:
-        cursor.execute("""
-        WITH costo_produccion AS (
-            SELECT  P.id producto_id,
-                    COALESCE(SUM(PUI.utiliza_inventario * I.precio_lote / I.tamano_lote), 0.0) AS costo
-            FROM    inventario_producto AS P
-                    LEFT JOIN inventario_productoutilizainventario AS PUI
-                        ON P.id = PUI.producto_id
-                    LEFT JOIN inventario_inventario AS I
-                        ON PUI.inventario_id = I.id
-            GROUP   BY 1
-            ORDER   BY 1 ASC
+
+class ProductoViewSet(GetWithOrmMixin, ModelViewSet):
+    queryset = Producto.objects.filter(is_active=True)
+    serializer_class = ProductoSerializer
+    permission_classes = [IsAuthenticated]
+
+    orm_fields = ['codigo', 'descripcion', 'abreviado', 'categoria', 'is_active']
+
+    def list(self, request):
+        qs = (
+            self.queryset.annotate(
+                precio_con_iva=Coalesce(F('intervalos__precio_con_iva'), F('gran_formato__precio_m2')),
+                costo_prod=Sum(F('inventarios__utiliza_inventario') * F('inventarios__inventario__precio_lote') / F('inventarios__inventario__tamano_lote'))
+            )
+            .annotate(
+                descripcion_=Concat(
+                    F("descripcion"),
+                    Case(When(intervalos__desde__gt=1, then=Concat(Value(', desde '), Cast(F('intervalos__desde'), CharField()), Value(' unidades')))),
+                    Case(When(intervalos__duplex=True, then=Value(' [PRECIO DUPLEX]')))
+                ),
+                precio_sin_iva=F('precio_con_iva') / 1.16,
+                utilidad=F('precio_con_iva') - F('costo_prod')
+            )
+            .order_by('pk', 'intervalos__desde', 'intervalos__duplex')
+            .values('id', 'codigo', 'descripcion_', 'abreviado', 'categoria', 'precio_con_iva', 'precio_sin_iva', 'costo_prod', 'utilidad')
         )
-        SELECT  P.id,
-                P.codigo,
-                CASE
-                    WHEN P_Inv.producto_id IS NOT NULL THEN
-                        P.descripcion
-                        || CASE 
-                            WHEN desde > 1 
-                                THEN ', desde ' || ROUND(desde::numeric, 0)::text || ' unidades ' 
-                            ELSE '' 
-                        END
-                        || CASE 
-                            WHEN P_Inv.duplex 
-                                THEN '[PRECIO DUPLEX]' 
-                            ELSE '' 
-                        END
-                    ELSE 
-                        P.descripcion
-                END                                                             AS descripcion,
-                P.abreviado,
-                COALESCE(P_Inv.precio_con_iva, P_gran.precio_m2)                AS precio_con_iva,
-                COALESCE(P_Inv.precio_con_iva, P_gran.precio_m2) / 1.16         AS precio_sin_iva,
-                C_Prod.costo                                                    AS costo_prod,
-                COALESCE(P_Inv.precio_con_iva, P_gran.precio_m2) - C_Prod.costo AS utilidad
-        FROM    inventario_producto AS P
-                LEFT JOIN inventario_productointervalo AS P_Inv
-                    ON P.id = P_Inv.producto_id
-                LEFT JOIN inventario_productogranformato AS P_gran
-                    ON P.id = P_gran.producto_id
-                JOIN costo_produccion AS C_Prod
-                    ON P.id = C_Prod.producto_id
-        WHERE   P.is_active = 'true'
-        ORDER   BY 1 ASC;
-        """)
-        cols = ['id', 'codigo', 'descripcion', 'precio_con_iva', 'precio_sin_iva', 'costo_prod', 'utilidad']
-        return Response(data=[dict(zip(cols, row)) for row in cursor.fetchall()], status=200)
+        return Response(data=qs, status=200)
